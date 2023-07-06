f2c308c3c8a7207880b96f29dfb57b178f093815
taminhduc2009 <bluebest0912@gmail>
2023-07-06 20:33:46 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        dynamicBullets: true,
+    },
+    autoplay: {
+        delay: 2000,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+})
